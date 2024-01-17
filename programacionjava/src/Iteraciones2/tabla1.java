09837031cf3eae7e63e9c57bf3a0c082a2dd5083
Javier2207 <javierpiru222@gmail.com>
2024-01-17 12:31:51 +0100

    tablas

---
+++ programacionjava/src/Iteraciones2/tabla1.java
@@ -0,0 +1,18 @@
+package Iteraciones2;
+
+import java.util.Arrays;
+
+public class tabla1 {
+
+	public static void main(String[] args) {
+		
+		int tabla1[] = {1,2,3,4,5,} ;
+		System.out.println(Arrays.toString(tabla1));
+		for(int i=0; i<tabla1.length; i++)
+		System.out.println(tabla1[i]);
+		
+		
+
+	}
+
+}
---
+++ programacionjava/src/Iteraciones2/tabla2.java
@@ -0,0 +1,33 @@
+package Iteraciones2;
+
+import java.util.Scanner;
+
+public class tabla2 {
+
+	public static void main(String[] args) {
+		
+		int tabla[]= {3,7,12,18,22};
+		boolean encontrado = false;
+		Scanner escaner=new Scanner(System.in);
+		
+		System.out.println("Introduzca numero que desea encontrar:");
+		int numero=escaner.nextInt();
+		
+	for(int i=0; i<tabla.length && !encontrado; i++) {
+		if(numero==tabla[i])
+			System.out.println(i);
+		
+		
+			
+			
+			
+	}
+			
+		
+		
+		
+		
+
+	}
+
+}
